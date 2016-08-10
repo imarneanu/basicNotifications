995f2bbd9a542b7409e6e2f173662198a9334385
imarneanu <iulia.marneanu@appsrise.com>
2016-08-10 17:27:40 +0300

    Basic notifications sample

---
+++ Application/src/main/java/com/example/android/basicnotifications/ReplyActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.basicnotifications;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by imarneanu on 8/10/16.
+ */
+public class ReplyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
